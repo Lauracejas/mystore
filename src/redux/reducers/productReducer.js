9f7efc2aa62333feb993aa1e454288b83931cdfa
lauracejas <lauracejasmartin94@gmail.com>
2021-05-17 14:42:32 -0400

    create components

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,19 +1,21 @@
 import { ActionTypes } from "../constants/action-types";
 
 const initialState = {
-    products: [{
-        id: 1,
-        title: "Dipesh",
-        category: "programer",
-    }]
+    products: [
+        {
+            id: 1,
+            title: "Dipesh",
+            category: "programer",
+        },
+    ],
 }
 
-export const productReducer = (state, {type, payload}) => {
-switch (type) {
-    case ActionTypes.SET_PRODUCTS:
-        return state;
-        
-    default:
-        return state;
-}
+export const productReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+
+        default:
+            return state;
+    }
 }
